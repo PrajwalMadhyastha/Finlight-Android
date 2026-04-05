16bfc4cb60ea79806cc43607229b5554320a4461
PrajwalMadhyastha <prajwalmadhyastha777@gmail.com>
2026-04-05 17:43:41 +0530

    Upgraded to AGP 9.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("com.android.application") version "8.13.2" apply false
+    id("com.android.application") version "9.1.0" apply false
     // Set the Kotlin version for the entire project.
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     // Set the KSP version for the entire project.
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
     // --- ADDED: Declare the Compose Compiler plugin for the project ---
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
     id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
