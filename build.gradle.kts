e6b3b37f6eb30864d9805a6897f66da4abb8de05
PrajwalMadhyastha <prajwalmadhyastha777@gmail.com>
2026-04-22 14:35:56 +0530

    Upgrade AGP dependency from 9.1.0 to 9.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application") version "9.1.0" apply false
+    id("com.android.application") version "9.1.1" apply false
     // Set the Kotlin version for the entire project.
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     // Set the KSP version for the entire project.
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
     // --- ADDED: Declare the Compose Compiler plugin for the project ---
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 
