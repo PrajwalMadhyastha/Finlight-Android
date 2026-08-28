aafc4a0b1ef6b3189add3ef1c2e4456af0aa9773
PrajwalMadhyastha <prajwalmadhyastha777@gmail.com>
2026-08-28 11:34:52 +0530

    style(merchant-rules): remove unused import and pass ktlintCheck

--- app/src/main/java/io/pm/finlight/ui/screens/ManageMerchantRulesScreen.kt
+++ app/src/main/java/io/pm/finlight/ui/screens/ManageMerchantRulesScreen.kt
@@ -1,7 +1,6 @@
 package io.pm.finlight.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
